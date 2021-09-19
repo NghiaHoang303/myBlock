a9ddf48a979ad605ee95a520089df7b64ba7c7b5
nghiahoangdayoneteams <nghiahoang@dayoneteams.com>
2021-09-19 15:32:55 +0700

    write backend with nodejs express multer bcript router

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const multer = require('multer'); 
+
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
+const categoryRoute = require('./routes/categories')
+
+
+
+dotenv.config()
+app.use(express.json())
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// }).then(console.log('Connect to mongoDb')) 
+// .catch((err) => console.log(err));
+mongoose.connect('mongodb://localhost:27017/my_blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+
+}).then(console.log('Connect to mongoDb')) 
+  .catch((err) => console.log(err));
+
+// app.use('/nghiahoang',(req, res) => {
+//     console.log("Hey this is Nghia Hoang url");
+// })
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images")
+  },
+  filename:(req, file, cb) => {
+    cb(null, req.body.name);
+  }
+});
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), 
+  (req, res) => {
+    res.status(200).json("file has been uploaded")
+  }
+)
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+// console.log('hello nghia hoang win every ');
+app.listen("5000", ()=> {
+    console.log('Backend is running...');
+});
